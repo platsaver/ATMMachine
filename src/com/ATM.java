7c0ae8160426f4ae0cff5eeff56f01ef8f07835d
platsaver <kingdomofhellborn123@gmail.com>
2024-10-21 16:53:50 +0700

    Add files via upload

---
+++ src/com/ATM.java
@@ -0,0 +1,29 @@
+package com;
+
+public class ATM {
+	private int balance; 
+	private int depositBalance; 
+	private int withdrawBalance; 
+	
+	public ATM(){
+		
+	}
+	int getBalance() {
+		return balance; 
+	}
+	void setBalance(int balance) {
+		this.balance = balance; 
+	}
+	int getDeposit() {
+		return depositBalance; 
+	}
+	void setDeposit(int depositBalance) {
+		this.depositBalance = depositBalance; 
+	}
+	int getWithdraw() {
+		return withdrawBalance; 
+	}
+	void setWithdraw(int withdrawBalance) {
+		this.withdrawBalance = withdrawBalance; 
+	}
+}
---
+++ src/com/OperationInterface.java
@@ -0,0 +1,8 @@
+package com;
+
+public  interface OperationInterface { // interface function as abstract, use to group related method
+	void viewBalance(); 
+	void withdrawAmount(int withdraw); 
+	void depositAmount(int deposit); 
+	void ViewMiniStatement(); 
+}
